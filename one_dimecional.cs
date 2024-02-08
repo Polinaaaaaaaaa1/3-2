014177484a2edfecd454034c61520b9f03abfe31
Polinaaaaaaaaa1 <158997747+Polinaaaaaaaaa1@users.noreply.github.com>
2024-02-08 20:57:55 +0500

    Update one_dimecional.cs

--- one_dimecional.cs
+++ one_dimecional.cs
@@ -6,7 +6,32 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp16
 {
-    internal class one_dimecional
+    public sealed class one_dimensional : Array
     {
-    }
+        public int[] arrayyy;
+        public one_dimensional(int[] array)
+        {
+            arrayyy = array;
+        }
+        public override void initialization()
+        {
+            for (int i = 0; i < arrayyy.Length; i++)
+            {
+                arrayyy[i] = int.Parse(Console.ReadLine());
+            }
+        }
+        public override void average_and_print()
+        {
+            int amount = 0;
+            int sum = 0;
+            for (int i = 0; i < arrayyy.Length; i++)
+            {
+                amount += 1;
+                sum += arrayyy[i];
+
+            }
+            Console.WriteLine($"среднее в одномерном массиве {sum / amount}");
+            Console.WriteLine("\nодномерный массив : {0}", String.Join(" ", arrayyy));
+        }
+    
 }
