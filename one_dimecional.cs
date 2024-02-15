3cc60f2f11958e8c28a22a5e78d6cf8602a0d5e6
Polinaaaaaaaaa1 <158997747+Polinaaaaaaaaa1@users.noreply.github.com>
2024-02-15 17:09:01 +0500

    Update one_dimecional.cs

--- one_dimecional.cs
+++ one_dimecional.cs
@@ -6,32 +6,41 @@ using System.Threading.Tasks;
 
 namespace задание_3_2_наследование_
 {
-    public sealed class one_dimensional : Array
-    {
+        public sealed class one_dimensional : Array
+        {
         public int[] arrayyy;
-        public one_dimensional(int[] array)
-        {
-            arrayyy = array;
-        }
-        public override void initialization()
-        {
-            for (int i = 0; i < arrayyy.Length; i++)
+        public int length;
+            public one_dimensional(int length)
             {
+            this.length = length;
+                arrayyy = new int[length];
+            }
+            public override void initialization()
+            {
+                for (int i = 0; i < arrayyy.Length; i++)
+                {
+                Console.WriteLine($"эл-т {i} в одномерном массиве");
                 arrayyy[i] = int.Parse(Console.ReadLine());
+                }
             }
-        }
-        public override void average_and_print()
-        {
-            int amount = 0;
-            int sum = 0;
-            for (int i = 0; i < arrayyy.Length; i++)
+            public override void average()
             {
-                amount += 1;
-                sum += arrayyy[i];
+                int amount = 0;
+                int sum = 0;
+                for (int i = 0; i < arrayyy.Length; i++)
+                {
+                    amount += 1;
+                    sum += arrayyy[i];
 
+                }
+                Console.WriteLine($"среднее в одномерном массиве {sum / amount}");
             }
-            Console.WriteLine($"среднее в одномерном массиве {sum / amount}");
+         public override void print()
+        {
+
             Console.WriteLine("\nодномерный массив : {0}", String.Join(" ", arrayyy));
         }
+
+    }
     
 }
