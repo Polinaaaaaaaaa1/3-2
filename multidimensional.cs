a48de7eb1d5109757236bb5841d43ce3783a426b
Polinaaaaaaaaa1 <158997747+Polinaaaaaaaaa1@users.noreply.github.com>
2024-02-15 17:10:13 +0500

    Update multidimensional.cs

--- multidimensional.cs
+++ multidimensional.cs
@@ -5,12 +5,16 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace задание_3_2_наследование_
-{public sealed class multidimensional : Array
+{
+    public sealed class multidimensional : Array
     {
         public int[,] arrayyy;
-        public multidimensional(int[,] array)
-        {
-            arrayyy = array;
+        public int length;
+        public int heigth;
+        public multidimensional(int length, int heigth)
+        {this.length = length;
+            this.heigth = heigth;
+            arrayyy = new int[length,heigth];
         }
         public override void initialization()
         {
@@ -20,14 +24,14 @@ namespace задание_3_2_наследование_
 
                 {
 
-                    Console.Write($"Элемент [{i},{j}]: ");
+                    Console.Write($"Элемент [{i},{j}]в многомерном массиве: ");
 
                     arrayyy[i, j] = Convert.ToInt32(Console.ReadLine());
 
                 }
             }
         }
-        public override void average_and_print()
+        public override void average()
         {
             int amount = 0;
             int sum = 0;
@@ -45,5 +49,17 @@ namespace задание_3_2_наследование_
             }
             Console.WriteLine($"среднее в многомерном массиве {sum / amount}");
         }
+        public override void print()
+        {
+            for (int i = 0; i < arrayyy.GetLength(0); i++)
+            {
+                for (int j = 0; j < arrayyy.GetLength(1); j++)
+                {
+                    Console.Write(arrayyy[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+        }
     }
 }
+
